4517430910d0bde4c133a4a9926e41065942cdb0
Jose Antonio Holgado <joseantonio-hb@hotmail.com>
2024-09-05 15:15:09 +0200

    Refactor: i use nomeclature BEM
    I applied the BEMnaming convention in the Navbar component and nested the code in Sass

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -10,18 +10,18 @@ const Navbar = () => {
   }
     return (
         <nav className="navbar">
-        <div className="logo">
+        <div className="navbar__logo">
           <a href="/">Jose A</a>
         </div>
 
        {/* Here we create a div for the found menu, that show with the clase 'menu-bg' */}
-       <div className={`menu-bg ${isOpen ? "active" : ""}`}></div>
+       <div className={`navbar__menu-bg ${isOpen ? "navbar__menu-bg--active" : ""}`}></div>
 
-        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/about">Habilidades</a></li>
-          <li><a href="/services">Servicios</a></li>
-          <li><a href="/contact">Contacto</a></li>
+        <ul className={`navbar__nav-links ${isOpen ? "navbar__nav-links--active" : ""}`}>
+          <li className="navbar__nav-item"><a href="/">Inicio</a></li>
+          <li className="navbar__nav-item"><a href="/about">Habilidades</a></li>
+          <li className="navbar__nav-item"><a href="/services">Servicios</a></li>
+          <li className="navbar__nav-item"><a href="/contact">Contacto</a></li>
         </ul>
        
         <BurguerButton toggleMenu={toggleMenu} isOpen={isOpen}/>
