596f96ad9802b97ec054a1830f758e2667365f6b
Jose Antonio <joseantonio-hb@hotmail.com>
2025-01-31 19:47:27 +0100

    Upload: I create new Component ContactForm

---
+++ portfolio/src/components/Section4/ContactForm/ContactForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ContactForm.scss"; 
+
+const ContactForm = () => {
+  return (
+    <form 
+      action="https://formsubmit.co/joseantonio-hb@hotmail.com" 
+      method="POST"
+      className="form-container"  
+    >
+      <input type="hidden" name="_next" value="https://tu-pagina.github.io/gracias.html" />
+      <input type="hidden" name="_captcha" value="false" />
+
+      <div>
+        <label>Nombre</label>
+        <input name="name" required />
+      </div>
+
+      <div>
+        <label>Email</label>
+        <input name="email" type="email" required />
+      </div>
+
+      <div>
+        <label>Mensaje</label>
+        <textarea name="message" required />
+      </div>
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
+
+export default ContactForm;
--- portfolio/src/components/Section4/Section4.jsx
+++ portfolio/src/components/Section4/Section4.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Section4.scss'
 import MyLottieAnimationEmail from './AnimationEmail/AnimationEmail'
+import ContactForm from './ContactForm/ContactForm'
 const Section4 = () => {
     return (
         <div className="section4">
@@ -8,8 +9,14 @@ const Section4 = () => {
                 <h2 className="section4__title">Contacto</h2>
                 <p className="section4__description">Si tienes alguna pregunta o quieres trabajar conmigo, no dudes en contactarme.</p>
             </div>
-            <div className="section4__animation">
-                <MyLottieAnimationEmail/>
+            <div className="section4__box">
+                <div className="section4__box-contact">
+                    <ContactForm/>
+                </div>
+
+                <div className="section4__box-animation">
+                    <MyLottieAnimationEmail/>
+                </div>
             </div>
         </div>
     )
